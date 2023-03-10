1b0a9741a37b29c1230fc103125f9c910b15bc46
Awais <awais381lead@gmail.com>
2023-03-10 12:36:28 +0500

    basic setup is done of graphql

---
+++ Backend/src/resolvers/hello.ts
@@ -0,0 +1,10 @@
+import { Resolver } from "type-graphql";
+import { Query } from "type-graphql/dist/decorators";
+
+@Resolver()
+export class HelloResolver {
+    @Query(() => String)
+    hello() {
+        return 'Hello World'
+    }
+}
--- Backend/src/server/index.ts
+++ Backend/src/server/index.ts
@@ -1,24 +1,32 @@
 import { MikroORM } from "@mikro-orm/core"
-import { Post } from "../entities/post";
 import { __prod__ } from "../constants"
 import mikroOrmConfig from "../mikro-orm.config"
+import express from "express"
+import { ApolloServer } from "apollo-server-express"
+import { buildSchema } from "type-graphql"
+import { HelloResolver } from "../resolvers/hello"
 
 const main = async () => {
 
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.isConnected()
-    // await orm.getMigrator().up();  // run the migrations every time server starts 
-    let emFork = orm.em.fork({})
-    const post = emFork.create(Post, {
-        title: "migrations are not being built",
-        createdAt: "",
-        updatedAt: ""
-    })
-    await emFork.persistAndFlush(post);
 
-    // Simple commands to get all the posts 
-    // const getAllPosts = await emFork.find(Post,{});
-    // console.log(getAllPosts)
+    const app = express();
+    app.use(express.json());
+    let port = 3000;
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+    
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
+    app.listen(port, () => {
+        console.log(`App is running .... port ${port}`)
+    })
+
 }
 
 main().catch(err => {
